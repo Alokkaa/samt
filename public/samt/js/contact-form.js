5048d1281a0af3b7651d8f5f7d8a0263e1500079
alokka <ali@alokka.com>
2019-02-02 00:17:12 +0200

    vedio r36

--- public/samt/js/contact-form.js
+++ public/samt/js/contact-form.js
@@ -46,34 +46,20 @@ $(document).ready(function($) {
     // }
 
 
-$('.zoom, .zoom-image').magnificPopup({
- // delegate: 'a',
- type: 'image',
- tLoading: 'Loading image #%curr%...',
- mainClass: 'mfp-img-mobile',
- gallery: {
- enabled: true,
- navigateByImgClick: true,
- preload: [0,1] // Will preload 0 - before current, and 1 after the current image
- },
- image: {
- tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
- /*titleSrc: function(item) {
- return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
- }*/
- },
- callbacks: {
- elementParse: function(item) {
- // the class name
- if(item.el.context.className == 'video') {
- item.type = 'iframe';
- } else {
- item.type = 'image';
- }
- }
- },
-});
-
+    $('.zoom, .zoom-image').magnificPopup({
+      type:'image',
+      removalDelay: 300,
+      mainClass: 'mfp-fade'
+    });
+     
+    $('.youtube').magnificPopup({
+      disableOn: 700,
+      type: 'iframe',
+      mainClass: 'mfp-fade',
+      removalDelay: 300,
+      preloader: false,
+      fixedContentPos: false
+    });
 
 });
 
